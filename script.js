66c503128b18452964ad5c8f27479d60d19787bb
Kristin Baumann <hello@kristin-baumann.com>
2025-12-10 09:41:53 +0100

    Fix container position in page

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 console.log("Test from script embed");
-const mainElement = document.querySelector("#main_content .content");
+
 const newDiv = document.createElement("div");
 newDiv.style.padding = "20px";
+newDiv.style.marginBottom = "20px";
 newDiv.style.border = "2px solid #000";
 newDiv.style.gridColumn = "span 12";
 newDiv.style.width = "auto";
 newDiv.style.maxWidth = "100%";
 newDiv.innerText = "Container for matrix visualization";
-mainElement.appendChild(newDiv);
+
+// Insert after a specified number of children
+const afterChildren = 1;
+const mainElement = document.querySelector("#main_content .content");
+const targetChild = mainElement.children[afterChildren];
+if (targetChild) {
+  mainElement.insertBefore(newDiv, targetChild);
+} else {
+  mainElement.appendChild(newDiv);
+}
